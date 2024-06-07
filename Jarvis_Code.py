1a4a114a79db2db5b3335e4df5d678ca69206eed
Avash Kusle <kusleavash@gmail.com>
2024-06-07 13:12:55 -0500

    speech recognition and audio playback working

---
+++ Jarvis_Code.py
@@ -0,0 +1,60 @@
+import subprocess
+import wolframalpha
+import pyttsx3
+import tkinter
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import pyjokes
+import feedparser
+import smtplib
+import ctypes
+import time
+import requests
+import shutil
+from twilio.rest import Client
+#from clint.textui import progress
+from ecapture import ecapture as ec
+from bs4 import BeautifulSoup
+import win32com.client as wincl
+from urllib.request import urlopen
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices') #gets you the details of the current voice
+engine.setProperty('voice', voices[0].id)  # 0-male voice , 1-female voice
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+     
+    r = sr.Recognizer()
+     
+    with sr.Microphone() as source:
+         
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+  
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language ='en-in')
+        print(f"User said: {query}\n")
+        speak(query)
+  
+    except Exception as e:
+        print(e)    
+        print("Unable to Recognize your voice.")  
+        return "None"
+     
+    return query
+
+takeCommand()
